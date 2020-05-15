c78b8b3289fff28754cb43f7581a12587fa750b3
aditya9729 <adityagudal29@gmail.com>
2020-05-15 11:28:12 -0400

    Added python scripts,dockerfile,config files and data

--- src/store_data_in_s3.py
+++ src/store_data_in_s3.py
@@ -2,26 +2,6 @@ import boto3
 from tqdm import tqdm
 import logging
 
-# READ DATA FROM S3 -- function not required
-def read_from_s3(bucket_name,files,write_location):
-	"""
-	Downloads data from s3 bucket and stores it
-	:param bucket_name: AWS S3 bucket name
-	:param files: Files to read
-	:param write_location:	path where we would write the files to
-	:return: None
-	"""
-
-	s3 = boto3.resource("s3")
-	bucket = s3.Bucket(bucket_name)
-
-	files_to_download=files
-
-	for file in tqdm(files_to_download):
-		bucket.download_file(str(file),write_location+str(file))
-
-
-
 # WRITE DATA TO S3
 def write_to_s3(bucket_name,access_key,secret_key,folder,files):
 	"""

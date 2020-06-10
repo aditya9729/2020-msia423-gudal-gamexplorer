8506ae3c64189cacda4f98e316ed37a3028f63fe
Aditya Gudal <adityagudal29@gmail.com>
2020-06-10 09:50:44 -0500

    Added  local/rds functionality in config/config.py

--- config/config.py
+++ config/config.py
@@ -12,8 +12,31 @@ SQLALCHEMY_ECHO = True  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
 
 # Database engine Connection string
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
+# For CREATING Local Database connection config
+local=True # change this for local sqlite vs rds instance database, don't forget to set env variables see above
+PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # change be changed
+DATABASE_PATH = os.path.join(PROJECT_HOME, 'data/customers.db')# name is customers.db by default
+
+
+# RDS instance Database configurations
+connection_type="mysql+pymysql" # for rds
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+host = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_PORT")
+database = os.environ.get("MYSQL_DATABASE")
+
+## This is to write data to RDS/local database
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+if SQLALCHEMY_DATABASE_URI is not None:
+    pass
+elif host is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/customers.db'
+else:
+    SQLALCHEMY_DATABASE_URI = '{dialect}://{user}:{pw}@{host}:{port}/{db}'.format(dialect=DB_DIALECT, user=DB_USER,
+                                                                                  pw=DB_PW, host=DB_HOST, port=DB_PORT,
+       
 SAVE_INTERMEDIATE_PATH='./data/external/intermediate.pkl'
 SAVE_MODEL_PATH='./models/als_model.joblib'
 
@@ -25,20 +48,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 RAW_DATA_FOLDER = "./data/external/"
 RAW_DATA_FILENAMES=['games_data.pkl']
 RAW_DATA_WRITE_LOCATION = "./data/external/"
-
-# RDS instance Database configurations
-connection_type="mysql+pymysql" # for rds
-user = os.environ.get("MYSQL_USER")
-password = os.environ.get("MYSQL_PASSWORD")
-host = os.environ.get("MYSQL_HOST")
-port = os.environ.get("MYSQL_PORT")
-database = os.environ.get("MYSQL_DATABASE")
-
-# Local Database connection config
-local=True # change this for local sqlite vs rds instance database, don't forget to set env variables see above
-PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # change be changed
-DATABASE_PATH = os.path.join(PROJECT_HOME, 'data/customers.db')# name is customers.db by default
-
 
 ## Configuration paths for src/featurize.py
 DATA_PATH = './data/external/games_data.pkl'
